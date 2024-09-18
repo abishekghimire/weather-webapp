57075bbdd2ba1c5f68e88bc2bc4fb529d6d1f5b5
abishekghimire <abishekghimire220.com>
2024-09-18 15:39:28 +0545

    added different display section

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const cityInput = document.querySelector(".city-input");
 const searchBtn = document.querySelector(".search-icon");
 
+const notFoundSection = document.querySelector(".not-found");
+const searchCitySection = document.querySelector(".search-city");
+const weatherInfoSection = document.querySelector(".weather-info");
+
+const apiKey = "7a28bd357a496ca21f14cd9c08f04186";
+
 searchBtn.addEventListener("click", () => {
   if (cityInput.value.trim() != "") {
-    updateWeatherInfo();
+    updateWeatherInfo(cityInput.value);
     cityInput.value = "";
     cityInput.blur();
   }
@@ -11,8 +17,31 @@ searchBtn.addEventListener("click", () => {
 //Key Event
 cityInput.addEventListener("keydown", (event) => {
   if (event.key == "Enter" && cityInput.value.trim() != "") {
-    updateWeatherInfo();
+    updateWeatherInfo(cityInput.value);
     cityInput.value = "";
     cityInput.blur();
   }
 });
+async function getData(endPoint, city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+  const response = await fetch(apiUrl);
+  return response.json();
+}
+async function updateWeatherInfo(city) {
+  const weatherData = await getData("weather", city);
+  if (weatherData.cod != 200) {
+    showDisplaySection(notFoundSection);
+    return;
+  }
+  console.log(weatherData);
+
+  showDisplaySection(weatherInfoSection);
+}
+
+function showDisplaySection(sectionToShow) {
+  [weatherInfoSection, searchCitySection, notFoundSection].forEach(
+    (section) => (section.style.display = "none")
+  );
+
+  sectionToShow.style.display = "block";
+}
