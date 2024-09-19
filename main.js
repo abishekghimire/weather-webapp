bfac06c596542edbfbde6e849d0f052f984d7860
abishekghimire <abishekghimire220.com>
2024-09-19 12:36:44 +0545

    added timezones, section handling

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ const notFoundSection = document.querySelector(".not-found");
 const searchCitySection = document.querySelector(".search-city");
 const weatherInfoSection = document.querySelector(".weather-info");
 
+const locationText = document.querySelector(".location");
+const tempText = document.querySelector(".temperature");
+const feelsLikeText = document.querySelector(".feels-like");
+const conditionText = document.querySelector(".condition");
+const weatherSummaryImg = document.querySelector(".weather-summary-img");
+const dateTimeText = document.querySelector(".date-time");
+
 const apiKey = "7a28bd357a496ca21f14cd9c08f04186";
 
+//handling search button
 searchBtn.addEventListener("click", () => {
   if (cityInput.value.trim() != "") {
     updateWeatherInfo(cityInput.value);
@@ -14,6 +22,7 @@ searchBtn.addEventListener("click", () => {
     cityInput.blur();
   }
 });
+
 //Key Event
 cityInput.addEventListener("keydown", (event) => {
   if (event.key == "Enter" && cityInput.value.trim() != "") {
@@ -27,17 +36,62 @@ async function getData(endPoint, city) {
   const response = await fetch(apiUrl);
   return response.json();
 }
+//function to change weather icon acc. to weather
+function getWeatherIcon(id) {
+  if (id <= 232) return "thunderstorm.png";
+  if (id <= 321) return "drizzle.png";
+  if (id <= 531) return "rain.png";
+  if (id <= 622) return "snow.png";
+  if (id <= 781) return "atmosphere.png";
+  if (id <= 800) return "clear.png";
+  else return "clouds.png";
+}
+//function to update weather info acc. to city
 async function updateWeatherInfo(city) {
   const weatherData = await getData("weather", city);
   if (weatherData.cod != 200) {
     showDisplaySection(notFoundSection);
     return;
   }
-  console.log(weatherData);
+
+  //fetching info from api
+  const {
+    name: location,
+    main: { temp, feels_like },
+    weather: [{ id, main }],
+    timezone,
+  } = weatherData;
+
+  locationText.innerHTML = `<i class="fa-solid fa-location-dot fa-xs" style="color: #000000"></i> ${location}
+`;
+  tempText.textContent = Math.round(temp) + "°C";
+  feelsLikeText.textContent = "Feels like " + Math.round(feels_like) + "°C";
+  conditionText.textContent = main;
+  weatherSummaryImg.src = `assets/weather/${getWeatherIcon(id)}`;
+
+  const localTime = getLocalTime(timezone);
+  dateTimeText.innerHTML = `<p>${localTime}`;
 
   showDisplaySection(weatherInfoSection);
 }
 
+//function to get time acc. to timezone
+function getLocalTime(timezoneOffset) {
+  const utcTime = new Date().getTime();
+  const localTimeInMs = utcTime + timezoneOffset * 1000;
+  const localTime = new Date(localTimeInMs);
+
+  return localTime.toLocaleString("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+}
+
+//function to handle different sections
 function showDisplaySection(sectionToShow) {
   [weatherInfoSection, searchCitySection, notFoundSection].forEach(
     (section) => (section.style.display = "none")
